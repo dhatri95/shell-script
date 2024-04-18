80b9e1838bad6970834824ff1f0bd00144751261
Dhatri <dhatri.venuturla@gmail.com>
2024-04-19 03:28:00 +0530

    inputs from cmd

---
+++ read-from-cmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter your username"
+read Username #-s will make the text entered not to be seen in cmd
+echo "Enter password"
+read Password
+
+echo "Username: $Username, Password: $Password"
+
+echo "Enter a digit"
+read num1 #-s will make the text entered not to be seen in cmd
+echo "Enter second digit"
+read num2
+
+echo "Sum of $num1 and $num2 is ${{$num1+$num2}}"
