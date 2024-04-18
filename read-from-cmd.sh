a1eb00c8076289e65b51ebbf1475a6a6f09249ed
Dhatri <dhatri.venuturla@gmail.com>
2024-04-19 03:30:18 +0530

    inputs from cmd

--- read-from-cmd.sh
+++ read-from-cmd.sh
@@ -3,7 +3,7 @@
 echo "Enter your username"
 read Username #-s will make the text entered not to be seen in cmd
 echo "Enter password"
-read Password
+read -s Password
 
 echo "Username: $Username, Password: $Password"
 
@@ -12,4 +12,4 @@ read num1 #-s will make the text entered not to be seen in cmd
 echo "Enter second digit"
 read num2
 
-echo "Sum of $num1 and $num2 is ${{$num1+$num2}}"
+echo "Sum of $num1 and $num2 is $(($num1+$num2))"
