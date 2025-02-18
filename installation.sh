8b4891c9d8ea38e2bd9bb9fa2b9cf9db07e02ebe
Dhatri <dhatri.venuturla@gmail.com>
2025-02-18 11:33:35 +0530

    installtion script

--- installation.sh
+++ installation.sh
@@ -6,6 +6,29 @@ Userid=$( id -u)
 if [ $Userid -ne 0 ]
    then
     echo "Please use sudo access to run the script"
+    exit 1
    else
     echo "You are using sudo access to run the script"
 fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+   then
+    echo "Installation succeeded"
+   else
+    echo "Installation Failed"
+    exit 1
+fi
+
+dnf install gitt -y
+
+if [ $? -eq 0 ]
+   then
+    echo "Installation succeeded"
+   else
+    echo "Installation Failed"
+    exit 1
+fi
+
+echo "Is script proceeding even if failed?"
