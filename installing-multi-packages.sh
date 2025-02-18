07f05476a1d221af331373a2956bdf87acbefddb
Dhatri <dhatri.venuturla@gmail.com>
2025-02-18 17:03:06 +0530

    added double quotes for R G Y variables

--- installing-multi-packages.sh
+++ installing-multi-packages.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 Userid=$(id -u)
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $Userid -eq 0 ]
     then
         echo -e "You are running scripts with $G SUDO Access $N"
     else
-        echo -e "Please use SUDO access to run the script"
+        echo -e "Please use $R SUDO access $N to run the script"
         exit 1
 fi
