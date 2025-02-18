e0615d34ca53cb624455e24dc5a96f92f017c480
Dhatri <dhatri.venuturla@gmail.com>
2025-02-18 16:57:33 +0530

    for, color, redirecting log all functions implementation

---
+++ installing-multi-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Userid=$(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+if ( $Userid -eq 0 )
+    then
+        echo -e "You are running scripts with $G SUDO Access $N"
+    else
+        echo -e "Please use SUDO access to run the script"
+        exit 1
+fi
