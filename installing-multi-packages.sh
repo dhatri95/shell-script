9c834d188a8503c75be109fe728da6d658ec2f7c
Dhatri <dhatri.venuturla@gmail.com>
2025-02-18 17:13:50 +0530

    added log and for loop

--- installing-multi-packages.sh
+++ installing-multi-packages.sh
@@ -5,6 +5,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+timestamp=$(date +%f-%H-%M-%S)
+ScriptName=$(echo $0 | cut -d "." -f 1)
+log=/tmp/$ScriptName-$timestamp.log
 
 if [ $Userid -eq 0 ]
     then
@@ -13,3 +16,8 @@ if [ $Userid -eq 0 ]
         echo -e "Please use $R SUDO access $N to run the script"
         exit 1
 fi
+
+for i in ($@)
+do
+    echo "Installing $i "
+done
