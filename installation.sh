28ed549a057e72586e796a728def1d8e15451f76
Dhatri <dhatri.venuturla@gmail.com>
2025-02-18 11:28:10 +0530

    installation script

---
+++ installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#installation of mysql, git
+
+Userid=$( id -u)
+if [ $Userid -ne 0]
+   then
+    echo "Please use sudo access to run the script"
+   else
+    echo "You are using sudo access to run the script"
+fi
