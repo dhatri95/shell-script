4b6ac69bfc5ad7f1ef124668bbcabaa24be963ff
Dhatri <dhatri.venuturla@gmail.com>
2025-02-18 10:30:00 +0530

    added if conditon

--- array.sh
+++ array.sh
@@ -3,10 +3,11 @@
 Numbers=(1 4 6 9 4 6 2 3 5 8)
 sum=0
 
-for i in ${Numbers[@]};
+for i in ${Numbers[@]}
 do
-  if (( i%2 == 0 )); then
-    sum= $((sum+i))
+  if [ i%2 -eq 0 ]
+   then
+   sum= $((sum+i))
   fi
 done
 
